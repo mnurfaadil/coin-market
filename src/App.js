eeabe33b3efe975e30f7cae6e55895bf587a2d18
Muhammad Nurfaadil <nfadil.mu@gmail.com>
2021-04-12 13:05:01 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Link } from "react-router-dom";
+import { 
+  HashRouter, 
+  Route
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  Accordion,
-  Card,
-  Tabs,
-  Tab,
   Navbar,
   Nav,
-  NavDropdown,
   Form,
   FormControl,
-  Table,
   Button
 } from 'react-bootstrap';
 
@@ -22,24 +19,12 @@ class App extends Component {
         <div>
           <header>
             <Navbar expand="lg" variant="dark" bg="dark">
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                   <Nav.Link href="#/">Home</Nav.Link>
                   <Nav.Link href="#/about">About</Nav.Link>
-                  {/* <Link to="/">Home</Link> */}
-                  {/* <Nav.Link href="topics">Topics</Nav.Link>
-                  <Nav.Link href="place">Places</Nav.Link>
-                  <Nav.Link href="contact">Contact</Nav.Link>
-                  <Nav.Link href="about">About</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item>Action</NavDropdown.Item>
-                    <NavDropdown.Item>Another action</NavDropdown.Item>
-                    <NavDropdown.Item>Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown> */}
                 </Nav>
                 <Form inline>
                   <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -48,8 +33,6 @@ class App extends Component {
               </Navbar.Collapse>
             </Navbar>
           </header>
-
-          <hr />
         </div>
 
         <Route exact path="/" component={Home} />
